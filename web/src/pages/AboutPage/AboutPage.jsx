bcfa9f0cd6819204ee9acf4bac24e22a10b8e00f
crohn44 <notmeae@gmail.com>
2024-05-30 14:33:14 -0400

    Chpater 1 complete

---
+++ web/src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,12 @@
+import { Metadata } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <Metadata title="About" description="About page" />
+      <p>This is the about section!</p>
+    </>
+  )
+}
+
+export default AboutPage
